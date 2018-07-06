bef73a9a9bf59c52bfee88e3113c0a2618dfd63d
ys-xps13 <goldhong@163.com>
2018-07-06 17:00:46 +0800

    kaishi-2450

---
+++ j5.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dell on 2018/7/6.
+ */
+// var arr=['hoo','hi','day','up']
+// console.log((arr.splice(2,2,"jiji")))
+// console.log(arr)
+console.log(!false)
+console.log(!345)
+console.log(!0)
